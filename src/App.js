edb82b472c195b9486380991acb6466f47bdb5b7
Nizami Sevindi <nsevindi87@gmail.com>
2022-10-14 22:43:13 +0200

    add Basket

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ import Product from './components/Product'
 function App() {
 
   const [money, setMoney] = useState(100)
+  const [basket, setBasket] = useState([])
+
+  useEffect(()=>{
+    console.log(basket)
+  }, [basket])
 
   return (
     <>
       <Header pMoney={money}/>
-      
+
       {products.map(product => (
-        <Product pProduct={product}/>
+        <Product key={product.id} pBasket={basket} pSetBasket={setBasket} pProduct={product}/>
       ))}
     
     </>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,45 @@
 import React from 'react'
 
-function Product({pProduct}) {
+function Product({pProduct, pBasket, pSetBasket}) {
+
+  const basketItem = pBasket.find(item => item.id === pProduct.id)
+
+
+
+  const addBasket = () =>{
+    const checkBasket = pBasket.find(item => item.id === pProduct.id)
+
+    if (checkBasket){
+      checkBasket.amount += 1
+      pSetBasket([...pBasket.filter(item => item.id !== pProduct.id), checkBasket])
+    }else{
+      pSetBasket([...pBasket, {
+        id: pProduct.id,
+        amount:1
+      }])
+    }
+  }
+
+
+
+
   return (
     <div>
       <div className="product">
+
         <h6>{pProduct.title}</h6>
+
         <div className="price">$ {pProduct.price}</div>
+
         <div className="actions">
           <button>Sat</button>
-          <span className="amount">0</span>
-          <button>Satin Al</button>
+          <span className="amount">{basketItem && basketItem.amount || 0}</span>
+          <button onClick={addBasket}>Ekle</button>
         </div>
+
+
+
+
         <style jsx>
           {`
           .product{
