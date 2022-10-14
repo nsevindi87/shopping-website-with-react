6596d0716e1143e4b99066ae36f75aa52326a04c
Nizami Sevindi <nsevindi87@gmail.com>
2022-10-14 22:10:42 +0200

    product section

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,24 @@ import React from 'react'
 function Product({pProduct}) {
   return (
     <div>
-        <h3>{pProduct.title}</h3>
+      <div className="product">
+        <h6>{pProduct.title}</h6>
+        <div className="price">$ {pProduct.price}</div>
+        <div className="actions">
+          <button>Sat</button>
+          <span className="amount">0</span>
+          <button>Satin Al</button>
+        </div>
+        <style jsx>
+          {`
+          .product{
+            padding:10px;
+            background:#fff;
+            border: 1px solid #ddd;
+            margin-bottom:20px;
+          }`}
+        </style>
+      </div>
     </div>
   )
 }
